d5ef4daa08bfb88bb816b8d18cc37494a22b76b5
areej fatima <areejfatima2954@gmail.com>
2025-10-03 20:01:18 +0500

    Removed fypDemo/Backend folder to clean repo

--- Backend/fypDemo/Backend/Backend/handler/routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import users from "../routes/users.js";
-
-const routes = (app) => {
-  app.use(express.json());
-  app.use("/users", users);
-};
-
-export default routes;
